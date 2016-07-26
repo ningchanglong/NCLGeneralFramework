4a2a191f725df505c8ccb773453519e1d65fc175
ningchanglong <ioi@ioidemac-mini-2.local>
2016-07-26 16:01:21 +0800

    Initial Commit

---
+++ NCLGeneralFramework/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NCLGeneralFramework
+//
+//  Created by ioi on 16/7/26.
+//  Copyright © 2016年 ncl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
